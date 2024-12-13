049ea5760e364b99f9a66b56cd0b49648fdc99e8
Aiken Ong <aiken.ong@u.nus.edu>
2024-12-13 10:01:19 +0800

    Completed task 2

--- src/main/java/vttp/batch5/ssf/noticeboard/controllers/NoticeController.java
+++ src/main/java/vttp/batch5/ssf/noticeboard/controllers/NoticeController.java
@@ -1,7 +1,54 @@
 package vttp.batch5.ssf.noticeboard.controllers;
 
+import java.util.logging.Logger;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import jakarta.validation.Valid;
+import vttp.batch5.ssf.noticeboard.models.Notice;
+
 // Use this class to write your request handlers
 
+@Controller
+@RequestMapping
 public class NoticeController {
+    
+    private final Logger logger = Logger.getLogger(NoticeController.class.getName());
+    
+    @GetMapping
+    public ModelAndView getNoticePage() {
+        ModelAndView mav = new ModelAndView();
 
+        Notice notice = new Notice();
+
+        logger.info("A visitor has reached the notice page.");
+
+        mav.addObject("notice", notice);
+        mav.setViewName("notice");
+
+        return mav;
+    }
+
+    @PostMapping("/notice")
+    public ModelAndView postNotice(
+        @Valid Notice notice,
+        BindingResult bindings
+    ) {
+        ModelAndView mav = new ModelAndView();
+
+        if (bindings.hasErrors()) {
+
+            logger.info("Invalid input for notice form.");
+            
+            mav.setViewName("notice");
+            return mav;
+        }
+
+        return mav;
+    }
 }
---
+++ src/main/java/vttp/batch5/ssf/noticeboard/models/Notice.java
@@ -0,0 +1,52 @@
+package vttp.batch5.ssf.noticeboard.models;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Future;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
+
+public class Notice {
+
+    @NotEmpty(message="Title cannot be empty")
+    @Size(min=3, max=128, message="Length must be between 3 and 123 characters")
+    private String title;
+
+    @NotEmpty(message = "Email cannot be empty")
+    @Email(message="Must be a well-formed email address")
+    private String poster;
+
+    @Future(message="Post date must be future dates")
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private LocalDate postDate;
+
+    @Size(min=1, message="Must include at least 1 category")
+    private List<String> categories;
+
+    @NotEmpty(message="Content of notice cannot be empty")
+    private String text;
+
+    public String getTitle() {return title;}
+    public void setTitle(String title) {this.title = title;}
+
+    public String getPoster() {return poster;}
+
+    public void setPoster(String poster) {this.poster = poster;}
+
+    public LocalDate getPostDate() {return postDate;}
+
+    public void setPostDate(LocalDate postDate) {this.postDate = postDate;}
+
+    public List<String> getCategories() {return categories;}
+
+    public void setCategories(List<String> categories) {this.categories = categories;}
+
+    public String getText() {return text;}
+
+    public void setText(String text) {this.text = text;}
+    
+}
